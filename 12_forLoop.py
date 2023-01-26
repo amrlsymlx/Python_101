5dfd30be2bc85bbeac7d34b09b02317dede0135e
A.Syamil <112838819+amrlsymlx@users.noreply.github.com>
2023-01-26 23:02:25 +0800

    commit

---
+++ 12_forLoop.py
@@ -0,0 +1,25 @@
+# for letter in "Amirul Syamil":
+#     print(letter)
+
+# for index in range(10):
+#     print(index)
+
+# for index in range(3, 47):
+#     print(index)
+
+friends = ["Ali", "Abu" , "Bakar" , "John" , "Emma", "Amin"]
+
+# for friend in friends:
+#     print(friend)
+
+# for index in range(len(friends)):
+    # print(friends[index])
+
+# for index in range(len(friends)):
+#     print(index)
+
+for index in range(5):
+    if index == 0:
+        print("First Iteration")
+    else:
+        print("Not first")
---
+++ 13_ExponentFunction.py
@@ -0,0 +1,6 @@
+print(2*3)
+print(2**3)
+
+def raise_to_power (base_num, pow_num):
+    result = 1
+    
