91908cb62562fd5366b1c089a105ab8b91e9c432
A.Syamil <112838819+amrlsymlx@users.noreply.github.com>
2023-01-27 12:24:08 +0800

    commit

---
+++ 17_readingFiles.py
@@ -0,0 +1,11 @@
+
+
+employee_file = open("employees.txt","r")
+
+print(employee_file.readable())
+print(employee_file.readline())
+print(employee_file.readline())  
+print(employee_file.read())
+
+
+employee_file.close()
