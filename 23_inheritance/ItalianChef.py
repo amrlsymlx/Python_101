72b0ca2ee4b01b012287d0e5cb6657f0e016a818
A.Syamil <112838819+amrlsymlx@users.noreply.github.com>
2023-01-27 16:58:06 +0800

    commit

---
+++ 23_inheritance/ItalianChef.py
@@ -0,0 +1,9 @@
+from Chef import chef
+
+class italianChef(chef):
+
+    def make_special_dish(self):
+        print("The chef makes chicken pasta")
+
+    def make_pasta(self):
+        print("The chef makes pasta")
---
+++ 23_inheritance/app.py
@@ -0,0 +1,14 @@
+from Chef import chef
+from ChineseChef import chineseChef
+from ItalianChef import italianChef
+
+myChef = chef()
+myChef.make_special_dish()
+
+myChineseChef = chineseChef()
+myChineseChef.make_special_dish()
+myChineseChef.make_fried_rice()
+
+myItalianChef = italianChef()
+myItalianChef.make_special_dish()
+myItalianChef.make_pasta()
