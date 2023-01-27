b1043fcfca96d62ec676684a676c74e453c2f8bf
A.Syamil <112838819+amrlsymlx@users.noreply.github.com>
2023-01-27 15:05:18 +0800

    commit

---
+++ 18_2_writeFiles.py
@@ -0,0 +1,7 @@
+html_file = open("index.html","w")
+
+
+html_file.write("<p>This is HTML</p>")
+
+
+html_file.close()
---
+++ 18_writeFiles.py
@@ -0,0 +1,19 @@
+
+
+employee_file = open("employees.txt","a")
+
+
+employee_file.write("\nToby - Human Resources")
+
+
+employee_file.close()
+
+# if mode "w" whole file will be rewrited
+
+#mode
+'''
+"w" - write
+"r" - read
+"r+" - read & write
+"a" - append
+'''
